6cc16b29160f4e7694989d7ca82fc09d422ec06e
Marco Prescher <m.prescher@gantner-instruments.com>
2023-10-03 19:24:27 +0200

    added sw.js and offline.html

--- chat-pwa/client/src/Root.js
+++ chat-pwa/client/src/Root.js
@@ -3,7 +3,22 @@
  */
 
 document.addEventListener('DOMContentLoaded', () => {
-    ChatApp.init().then(() => {
-        console.log("init");
-    });
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js')
+            .then((registration) => {
+                console.log(`Success: ${registration.scope}`);
+            })
+            .catch((error) => {
+                console.warn(`Error: registration failed with ${error}`);
+            })
+            .finally(() => {
+                ChatApp.init().then(() => {
+                    console.log("init");
+                });
+            });
+    } else {
+        ChatApp.init().then(() => {
+            console.log("init");
+        });
+    }
 }, false);
---
+++ chat-pwa/client/sw.js
@@ -0,0 +1,59 @@
+const VERSION = 1;
+const ASSETS_CACHE_PREFIX = "mpr-pwa-assets";
+const ASSETS_CACHE_NAME = `${ASSETS_CACHE_PREFIX}-${VERSION}`;
+const ASSET_URLS = [
+    "css/layout.css",
+    "index.html",
+    "offline.html",
+    "http://localhost:5000/images/daniel.jpg",
+    "http://localhost:5000/images/manuel.jpg",
+    "http://localhost:5000/images/guenther.jpg",
+    "http://localhost:5000/images/franz.jpg"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(ASSETS_CACHE_NAME)
+            .then((cache) => cache.addAll(ASSET_URLS))
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    const {request} = event;
+    const path = new URL(request.url).pathname;
+
+    if (ASSET_URLS.find((item) => request.url === item || path.includes(item))) {
+        event.respondWith((async function () {
+            const cache = await caches.open(ASSETS_CACHE_NAME);
+            const cachedResponse = await cache.match(request);
+
+            if (cachedResponse) {
+                if (cachedResponse.url.endsWith("index.html")) {
+                    return await fetch(request)
+                        .catch(() => {
+                            return caches.match("./offline.html");
+                        });
+                } else {
+                    return cachedResponse;
+                }
+            }
+
+            const response = await fetch(request);
+            await cache.put(request, response.clone());
+
+            return response;
+        })());
+    }
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(async function () {
+        const keys = await caches.keys();
+
+        return Promise.all(keys.map((key) => {
+            if (key.startsWith(ASSETS_CACHE_PREFIX) && key !== ASSETS_CACHE_NAME) {
+                return caches.delete(key);
+            }
+        }));
+    }());
+});
