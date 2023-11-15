eaccf17dc8d2bec6669aabda9a01526a7d6ce338
Marco Prescher <m.prescher@gantner-instruments.com>
2023-11-15 19:21:06 +0100

    added pwa-locator again

---
+++ pwa-locator/src/locator.js
@@ -0,0 +1,96 @@
+import cameraImage from './camera.svg';
+
+const COORD_FORMATTER = Intl.NumberFormat('de-DE', { minimumFractionDigits: 6, maximumFractionDigits: 6, minimumIntegerDigits: 3, style: 'unit', unit: 'degree' });
+const DIST_FORMATTER = Intl.NumberFormat('de-DE', { minimumFractionDigits: 1, maximumFractionDigits: 1, style: 'unit', unit: 'meter' });
+const DEG_FORMATTER = Intl.NumberFormat('de-DE', { minimumFractionDigits: 1, maximumFractionDigits: 1, style: 'unit', unit: 'degree' });
+
+const LOCATION_LEFT_ID = 'location-left';
+const LOCATION_MIDDLE_ID = 'location-middle';
+const CAMERA_INPUT_ID = 'camera';
+
+//map state
+var map;
+var ranger;
+
+function isTouchDevice() {
+    return (('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0));
+}
+
+function configureMap(latLngArray) {
+    map = L.map('map').setView(latLngArray, 17);
+    if (isTouchDevice()) {
+        map.removeControl(map.zoomControl);
+    }
+    map.attributionControl.setPosition('bottomleft');
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    ranger = L.circle(latLngArray, { radius: 20.0 }).addTo(map);
+}
+
+function updatePosition(position) {
+    const locatorLeftDiv = document.getElementById(LOCATION_LEFT_ID);
+    const locatorMiddleDiv = document.getElementById(LOCATION_MIDDLE_ID);
+
+    const coords = position.coords;
+    console.debug(`got new coordinates: ${coords}`);
+    locatorLeftDiv.innerHTML = `
+        <dl>
+            <dt>LAT</dt>
+            <dd>${COORD_FORMATTER.format(coords.latitude)}</dd>
+            <dt>LONG</dt>
+            <dd>${COORD_FORMATTER.format(coords.longitude)}</dd>
+            <dt>ALT</dt>
+            <dd>${coords.altitude ? DIST_FORMATTER.format(coords.altitude) : '-'}</dd>
+        </dl>`;
+    locatorMiddleDiv.innerHTML = `
+        <dl>
+            <dt>ACC</dt>
+            <dd>${DIST_FORMATTER.format(coords.accuracy)}</dd>
+            <dt>HEAD</dt>
+            <dd>${coords.heading ? DEG_FORMATTER.format(coords.heading) : '-'}</dd>
+            <dt>SPD</dt>
+            <dd>${coords.speed ? DIST_FORMATTER.format(coords.speed) : '-'}</dd>
+        </dl>`;
+    var ll = [coords.latitude, coords.longitude];
+
+    map.setView(ll);
+
+    ranger.setLatLng(ll);
+    ranger.setRadius(coords.accuracy);
+}
+
+/* setup component */
+window.onload = () => {
+    const cameraButton = document.getElementById(CAMERA_INPUT_ID);
+    const queryParams = new URLSearchParams(window.location.search);
+
+    //setup UI
+    cameraButton.src = cameraImage;
+
+    //init leaflet
+    configureMap([47.406653, 9.744844]);
+
+    //init footer
+    updatePosition({ coords: { latitude: 47.406653, longitude: 9.744844, altitude: 440, accuracy: 40, heading: 45, speed: 1.8 } });
+
+    // setup service worker
+    const swDisbaled = (queryParams.get('service-worker') === 'disabled');
+    console.debug(`query param 'service-worker': ${queryParams.get('service-worker')}, disabled: ${swDisbaled}`);
+    if (!swDisbaled && 'serviceWorker' in navigator) {
+        navigator.serviceWorker.register(
+            new URL('serviceworker.js', import.meta.url),
+            { type: 'module' }
+        ).then(() => {
+            console.log('Service worker registered!');
+        }).catch((error) => {
+            console.warn('Error registering service worker:');
+            console.warn(error);
+        });
+    }
+
+}
---
+++ pwa-locator/src/serviceworker.js
@@ -0,0 +1,61 @@
+import { manifest } from '@parcel/service-worker';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { warmStrategyCache } from 'workbox-recipes';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+
+
+// Set up page cache
+const assetsCache = new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+        new CacheableResponsePlugin({
+            statuses: [0, 200],
+        })
+    ],
+});
+
+const imageCache = new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+        new CacheableResponsePlugin({
+            statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+            maxAgeSeconds: 24 * 60 * 60,
+            maxEntries: 100,
+        }),
+    ],
+});
+
+const tileCache = new CacheFirst({
+    cacheName: 'tile-cache',
+    plugins: [
+        new CacheableResponsePlugin({
+            statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+            maxEntries: 100,
+        }),
+    ],
+});
+
+console.debug(`serviceworker.js got manifest: ${manifest}`);
+
+warmStrategyCache({
+    urls: manifest.filter(urlPath => /\.(html|css|js)/.test(urlPath)),
+    strategy: assetsCache,
+});
+
+warmStrategyCache({
+    urls: manifest.filter(urlPath => /\.(png|svg)/.test(urlPath)),
+    strategy: imageCache,
+});
+
+registerRoute(
+    ({ url }) => {
+        console.debug(`registerRoute for url.hostname: ${url.hostname}`);
+        return url.hostname === 'tile.openstreetmap.org';
+    }, tileCache);
